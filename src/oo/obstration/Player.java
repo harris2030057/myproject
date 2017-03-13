75735ca3315a3b4e2dd3c1e3624587559e22f713
harris <harris2030057@gmail.com>
2017-03-13 14:38:07 +0800

    Player

---
+++ src/oo/obstration/Player.java
@@ -0,0 +1,15 @@
+package oo.obstration;
+
+public class Player {
+	String name ;
+	int height;
+	int weight;
+	String position;
+	public Player(String name,int height,int weight,String position){
+	 this.name = name;
+	 this.height = height;
+	 this.weight = weight;
+	 this.position = position;
+	 
+	}
+}
---
+++ src/oo/obstration/PlayerTest.java
@@ -0,0 +1,13 @@
+package oo.obstration;
+
+public class PlayerTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Player p1 = new Player("Dirk NOwitzki",210,120,"PF");
+		 
+		  
+	  }
+	}
+
+
