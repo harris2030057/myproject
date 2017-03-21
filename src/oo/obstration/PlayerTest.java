a34b2c8073999eabbb2ccb8aaad58622bbadcd2c
harris2030057 <harris2030057@gmail.com>
2017-03-21 14:04:58 +0800

    Update PlayerTest.java

--- src/oo/obstration/PlayerTest.java
+++ src/oo/obstration/PlayerTest.java
@@ -4,8 +4,10 @@ public class PlayerTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 Player p1 = new Player("Dirk NOwitzki",210,120,"PF");
-		 
+		 Player p1 = new Player("Dirk Nowitzki",213,120,"PF");
+		Player p2 = new Player("Harrison Barnes",205,110,"SF");
+		Player p3 = new Player("Seth Curry",188,85,"PG")				
+ 
 		  
 	  }
 	}
