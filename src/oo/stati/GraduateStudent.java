5dc87222b6c5e40dc49e1cd62c94fe53cf89cd2f
user <user@es302n31>
2017-05-02 15:32:30 +0800

    123

--- src/oo/stati/GraduateStudent.java
+++ src/oo/stati/GraduateStudent.java
@@ -1,10 +1,24 @@
 package oo.stati;
 
-public class GraduateStudent extends Student{
- int thesis;
- public GraduateStudent(int english, int math, int chinese){
- this.english = english ;
- this.math = math ;
- this.chinese = chinese;
- }
+public class GraduateStudent extends Student {
+	int thesis;
+
+	public GraduateStudent(int english, int math, int chinese) {
+		this.english = english;
+		this.math = math;
+		this.chinese = chinese;
+	}
+
+	public void print() {
+		System.out.println(getScore(english) + "\t" + getScore(math) + "\t" + getScore(chinese) + "\t");
+	}
+
+	private String getScore(int score) {
+		if (score < pass) {
+			return String.valueOf(score) + "*";
+		} else {
+			return String.valueOf(score);
+
+		}
+	}
 }
--- src/oo/stati/Tester.java
+++ src/oo/stati/Tester.java
@@ -3,17 +3,16 @@ package oo.stati;
 public class Tester {
 
 	public static void main(String[] args) {
+
 		Student stu1 = new Student(58, 98, 66);
 		Student stu2 = new Student(67, 56, 77);
+		Student stu3 = new Student(75, 99, 59);
 		GraduateStudent gts1 = new GraduateStudent(58, 98, 66);
 		stu1.pass = 70;
 		stu1.print();
 		stu2.print();
-	
-		
-		
-			
-		
-	
-		};
+		stu3.print();
+		GraduateStudent gstu= new GraduateStudent(60, 50, 77);
+		gstu.print();
+	};
 }
