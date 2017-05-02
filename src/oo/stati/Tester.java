bce09e030353db22966d112f57a518ff1ddf5500
user <user@es302n31>
2017-05-02 15:10:04 +0800

    123

--- src/oo/stati/Tester.java
+++ src/oo/stati/Tester.java
@@ -5,12 +5,15 @@ public class Tester {
 	public static void main(String[] args) {
 		Student stu1 = new Student(58, 98, 66);
 		Student stu2 = new Student(67, 56, 77);
+		GraduateStudent stu1 = new Student(58, 98, 66);
 		stu1.pass = 70;
 		stu1.print();
 		stu2.print();
-		GraduateStudent()
+	
+		
+		
+			
+		
 	
 		};
-}
-
 }
