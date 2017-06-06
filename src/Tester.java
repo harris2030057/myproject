b7d05c81da1914d899599007772bf44ff6c1a65a
user <user@es302n31>
2017-06-06 16:30:12 +0800

    123

---
+++ src/Tester.java
@@ -0,0 +1,12 @@
+import oo.abs2.GradeReport;
+import oo.abs2.Report;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		Report report = new Report();
+		GradeReport g = new GradeReport();
+
+	}
+
+}
