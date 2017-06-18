d46ce6790917814cc163f2bb77fc4f7e8b57ced6
harris2030057 <harris2030057@gmail.com>
2017-06-18 14:00:13 +0800

    Update Order.java

--- src/oo/mid/Order.java
+++ src/oo/mid/Order.java
@@ -6,7 +6,7 @@ public class Order {
 	String drinks;
 	float price;
 
-	public Order(String hamburger, String desser, String drinks, float price) {
+	public Order(String hamburger, String dessert, String drinks, float price) {
 		this.hamburger = hamburger;
 		this.dessert = dessert;
 		this.drinks = drinks;
